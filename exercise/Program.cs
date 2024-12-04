03ec9b13f90feeb1f0a4061a6bb3949b35dc92f5
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2024-12-04 21:33:43 +0200

    Created different version of program with logic

--- exercise/Program.cs
+++ exercise/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Program
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 0;
+            bool amirich = money > 1000;
+            if ( !amirich)
+            {
+                Console.WriteLine("go to work");
+            }
+            else
+            {
+                Console.WriteLine("relax");
+            }
+        }
+    }
--- exercise/obj/Debug/net8.0/exercise.AssemblyInfo.cs
+++ exercise/obj/Debug/net8.0/exercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2dd8fc5024add4e39d397efb4c2c4b12e80d792")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e2e6dbdd3dcdc05effa4cc3080a459b4efed98")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
