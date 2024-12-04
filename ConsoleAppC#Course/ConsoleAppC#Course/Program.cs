8ba91ae52515f760dcac069f19153d6aad3915b0
Roman Bratchykov <brathcykov.roman@chnu.edu.ua>
2024-12-04 13:33:47 +0200

    added new console app

---
+++ ConsoleAppC#Course/ConsoleAppC#Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Dapper;
+namespace HelloWorld { 
+public class Program
+    {
+        public static void Main()
+        {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+            using (IDbConnection dbConnection = new SqlConnection(connectionString))
+            {
+                dbConnection.Open();
+                string sqlCommand = "SELECT GETDATE()";
+                DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+                Console.WriteLine(rightNow);
+            }
+        }
+    }
+}
